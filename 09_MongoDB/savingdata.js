c929682876c98820bc3f2ff8b48beead00361d65
randyfortier <randy.fortier@gmail.com>
2018-03-20 18:31:14 -0400

    Added mongoDB demo

--- 09_MongoDB/savingdata.js
+++ 09_MongoDB/savingdata.js
@@ -34,7 +34,7 @@ app.use(session({
 }));
 
 // utility code
-var usernames = [];
+//var usernames = [];
 function userExists(toFind) {
   for (var i = 0; i < usernames.length; i++) {
     if (usernames[i] === toFind) {
@@ -95,26 +95,81 @@ app.get('/students', function(request, response) {
   reloadStudentList(request, response, '');
 });
 
+app.post('/deleteStudent', function(request, response) {
+  var sid = request.body.sid;
+  Student.remove({sid: sid}, function(error) {
+    if (error) {
+      console.log('error deleting student: ' + error);
+      reloadStudentList(request, response, 'Unabled to delete student');
+    } else {
+      reloadStudentList(request, response, 'Student deleted');
+    }
+  });
+});
+
+app.post('/addOrUpdateStudent', function(request, response) {
+  var sid = request.body.sid;
+  var firstName = request.body.firstName;
+  var lastName = request.body.lastName;
+  var gpa = parseFloat(request.body.gpa);
+
+  var studentData = {sid: sid,
+                     firstName: firstName,
+                     lastName: lastName,
+                     gpa: gpa};
+
+  Student.find({sid: sid}).then(function(results) {
+    if (results.length > 0) {
+      // update the student
+      Student.update({sid: sid},
+                     studentData,
+                     {multi: false},
+                     function(error, numAffected) {
+        if (error || numAffected != 1) {
+          console.log('Unable to update student: ' + error);
+          reloadStudentList(request, response, 'Unable to update student');
+        } else {
+          reloadStudentList(request, response, 'Student updated');
+        }
+      });
+    } else {
+      // save a new student
+      var newStudent = new Student(studentData);
+      newStudent.save(function(error) {
+        if (error) {
+          console.log('Unable to save student');
+          reloadStudentList(request, response, 'Unable to add student');
+        } else {
+          reloadStudentList(request, response, 'Student added');
+        }
+      });
+    }
+  });
+});
+
 app.get('/register', function(request, response) {
   response.render('register', {title: 'Register'});
 });
 
 app.post('/processRegistration', function(request, response) {
   var username = request.body.username;
+  var email = request.body.email;
   var password = request.body.pwd;
+  var hashedPassword = bcrypt.hashSync(password);
 
-  if (userExists(username)) {
-    response.render('register', {title: 'Register',
-                                 errorMessage: 'Username in use'});
-  } else {
-    usernames.push(username);
-
-    request.session.username = username;
-
-    response.render('registrationSuccess', {username: username,
-                                            title: 'Welcome aboard!'});
-  }
-
+  var newUser = new User({username: username,
+                          email: email,
+                          hashedPassword: hashedPassword});
+  newUser.save(function(error) {
+    if (error) {
+      console.log('Unable to register: ' + error);
+      response.render('register', {errorMessage: 'Unable to register user.'});
+    } else {
+      request.session.username = username;
+      response.render('registrationSuccess', {username: username,
+                                              title: 'Welcome aboard!'});
+    }
+  });
 });
 
 app.get('/login', function(request, response) {
@@ -126,16 +181,20 @@ app.post('/processLogin', function(request, response) {
   var username = request.body.username;
   var password = request.body.pwd;
 
-  if (userExists(username)) {
-    // login success
-    request.session.username = username;
-    response.render('loginSuccess', {username: username,
-                                     title: 'Login Success'});
-  } else {
-    // Login failed
-    response.render('login', {title: 'Please Log In',
-                              errorMessage: 'Login Incorrect'});
-  }
+  User.find({username: username}).then(function(results) {
+    if (results.length == 0) {
+      // Login failed
+      response.render('login', {title: 'Please Log In',
+                                errorMessage: 'Login Incorrect'});
+    } else {
+      // login success
+      if (bcrypt.compareSync(password, results[0].hashedPassword)) {
+        request.session.username = username;
+        response.render('loginSuccess', {username: username,
+                                         title: 'Login Success'});
+      }
+    }
+  });
 });
 
 app.get('/logout', function(request, response) {
